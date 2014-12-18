526416c0c5bd46f553ab82298f6cca47cf147e35
Suraj <suraj@forwardfoundation.org.uk>
2014-12-18 15:18:58 +0000

    fix migration typo

--- db/migrate/20141218135910_add_slug_to_organisations.rb
+++ db/migrate/20141218135910_add_slug_to_organisations.rb
@@ -1,6 +1,6 @@
 class AddSlugToOrganisations < ActiveRecord::Migration
   def change
-    # add_column :organisations, :slug, :string
-    add_index :oranisations, :slug, unique: true
+    add_column :organisations, :slug, :string
+    add_index :organisations, :slug, unique: true
   end
 end
