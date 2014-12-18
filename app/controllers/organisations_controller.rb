0692fac2d1e1362d94fcc58536c0459ad1a9475a
Suraj <suraj@forwardfoundation.org.uk>
2014-12-18 12:59:50 +0000

    Add organisation controller and views again

---
+++ app/controllers/organisations_controller.rb
@@ -0,0 +1,24 @@
+class OrganisationsController < ApplicationController
+  def new
+    @organisation = Organisation.new
+  end
+
+  def create
+    @organisation = Organisation.new(organisation_params)
+    if @organisation.save
+      redirect_to organisation_path(@organisation)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @organisation = Organisation.find(params[:id])
+  end
+
+  private
+
+  def organisation_params
+    params.require(:organisation).permit(:name, :contact_name, :contact_role, :contact_email)
+  end
+end
