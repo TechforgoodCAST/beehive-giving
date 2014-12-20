6bd92017b6717bda4e707e9ef571fa6c362f17c3
Suraj <suraj@forwardfoundation.org.uk>
2014-12-20 23:34:06 +0000

    Style sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get '/' => redirect('/login')
+  root 'sessions#new'
   get '/login' => 'sessions#new', as: 'login'
   post '/login' => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
+
+  match 'get-matched', to: 'organisations#new', via: :get, as: 'sign_up'
 
   # get '/:id' => 'organisations#show', as: 'organisation'
 
