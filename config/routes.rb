5a55bbb8e33909a02d0209e4e47cb1260009956c
Suraj <suraj@forwardfoundation.org.uk>
2015-01-22 16:36:30 +0000

    add pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
 
-  match 'get-matched', to: 'users#new', via: :get, as: 'sign_up'
+  # Pages
+  match 'tour', to: 'pages#tour', via: :get, as: 'tour'
 
+  # Sign up
   match '/welcome', to: 'signup#user', via: :get, as: 'signup_user'
   match '/your-organisation', to: 'signup#organisation', via: :get, as: 'signup_organisation'
   match '/your-profile', to: 'signup#profile', via: :get, as: 'signup_profile'
@@ -18,8 +20,6 @@ Rails.application.routes.draw do
   match '/your-profile', to: 'signup#create_profile', via: :post
 
   match '/new-funder', to: 'signup#create_funder', via: :post
-
-  # get '/:id' => 'organisations#show', as: 'organisation'
 
   resources :users
   resources :organisations, :funders, :recipients do
