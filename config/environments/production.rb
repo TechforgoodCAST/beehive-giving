69ed39c7ef84f4a8178cb98713a7defed15af846
Suraj <suraj@forwardfoundation.org.uk>
2015-02-06 12:44:33 +0000

    Update mailer asset config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { :host => "beehive-giving.herokuapp.com" }
+  config.action_controller.asset_host = "beehive-giving.herokuapp.com"
+  config.action_mailer.asset_host = config.action_controller.asset_host
 end
