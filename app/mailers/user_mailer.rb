f211fbf85d251796964342acdc713098bc5f08d4
Suraj <suraj@forwardfoundation.org.uk>
2015-02-06 12:37:32 +0000

    Update user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "support@beehivegiving.org"
+  default from: "\"Beehive\" <support@beehivegiving.org>"
   def welcome_email(user)
     @user = user
     mail(to: @user.user_email, subject: 'Welcome to Beehive!')
