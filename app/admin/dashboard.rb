9d0e085efa631d2728d404bb04b0ca20074c6900
Suraj <suraj@forwardfoundation.org.uk>
2015-02-08 18:55:16 +0000

    Update admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,72 +4,66 @@ ActiveAdmin.register_page "Dashboard" do
   content title: proc{ I18n.t("active_admin.dashboard") } do
 
     div class: "blank_slate_container" do
-      span class: "blank_slate" do
-        h3 'Users'
-        h1 User.all.count
-      end
-
-      span class: "blank_slate" do
-        h3 'Non-profits'
-        h1 Recipient.all.count
-      end
-
-      span class: "blank_slate" do
-        h3 'Funders'
-        h1 Funder.all.count
-      end
-
-      span class: "blank_slate" do
-        h3 'Grants'
-        h1 Grant.all.count
-      end
-
-      span class: "blank_slate" do
-        h3 'Feedback'
-        h1 Feedback.all.count
-      end
-
-      span class: "blank_slate" do
-        h3 'Requests'
-        h1 Feature.all.count
-      end
-    end
-
-    section "Recent Non-profits" do
-      table_for Recipient.order("created_at desc").limit(5) do
-        column :name do |recipient|
-          link_to recipient.name, [:admin, recipient]
+      div style: "float:left; width: 20%;" do
+        span class: "blank_slate" do
+          h3 'Users'
+          h1 User.all.count
         end
-        column :country
-        column :created_at
+      end
+
+      div style: "float:left; width: 50%;" do
+        span class: "blank_slate" do
+          h3 'Non-profits'
+          h1 Recipient.all.count
+        end
+
+        span class: "blank_slate" do
+          h3 'Profiles'
+          h1 Profile.all.count
+        end
+
+        span class: "blank_slate" do
+          h3 'Feedback'
+          h1 Feedback.all.count
+        end
+
+        span class: "blank_slate" do
+          h3 'Requests'
+          h1 Feature.all.count
+        end
+      end
+
+      div style: "float:left; width: 30%;" do
+        span class: "blank_slate" do
+          h3 'Funders'
+          h1 Funder.all.count
+        end
+
+        span class: "blank_slate" do
+          h3 'Grants'
+          h1 Grant.all.count
+        end
       end
     end
 
-    # div class: "blank_slate_container", id: "dashboard_default_message" do
-    #   span class: "blank_slate" do
-    #     span I18n.t("active_admin.dashboard_welcome.welcome")
-    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
-    #   end
-    # end
+    div style: "float:left; width: 50%; padding: 0 20px; box-sizing: border-box;" do
+      section "Recent Non-profits" do
+        table_for Recipient.order("created_at desc").limit(5) do
+          column :name do |recipient|
+            link_to recipient.name, [:admin, recipient]
+          end
+          column :country
+          column :created_at
+        end
+      end
+    end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    div style: "float:left; width: 50%; padding: 0 20px; box-sizing: border-box;" do
+      section "User Activation by week" do
+        @metric = Recipient.group_by_week(:created_at).count
+        render :partial => 'metrics/line_chart', :locals => {:metric => @metric}
+      end
+    end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
