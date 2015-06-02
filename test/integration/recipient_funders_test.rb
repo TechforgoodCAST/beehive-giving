8f622bbea326f9378733b7a1095604e41eb3b04d
Suraj <contact@suninthesky.co.uk>
2015-06-02 19:03:04 +0100

    recipient eligible badge funders index

--- test/integration/recipient_funders_test.rb
+++ test/integration/recipient_funders_test.rb
@@ -76,4 +76,70 @@ class RecipientFundersTest < ActionDispatch::IntegrationTest
     assert_equal "/comparison/#{@funder.slug}", current_path
   end
 
+  def eligible_badge
+    @recipient = create(:recipient, founded_on: "01/01/2005")
+    @funders, @restrictions = [], []
+
+    3.times do |i|
+      @funder = create(:funder, :active_on_beehive => true)
+      @funders << @funder
+      create(:funder_attribute, :funder => @funder, :funding_stream => "All")
+    end
+
+    3.times { |i| create(:profile, :organisation => @recipient, :year => 2015-i ) }
+
+    3.times { |i| @recipient.unlock_funder!(@funders[i]) }
+  end
+
+  test "no eligibility badge if questions remaining" do
+    eligible_badge
+
+    3.times do |i|
+      @restrictions << create(:restriction)
+      @funding_stream = create(:funding_stream, :restrictions => @restrictions, :funders => [@funders[i]])
+    end
+
+    2.times do |i|
+      @eligibility = create(:eligibility, :recipient => @recipient, :restriction => @restrictions[i])
+    end
+
+    create_and_auth_user!(:organisation => @recipient)
+    visit '/funders'
+
+    assert page.has_content?('Eligible', count: 2)
+    assert page.has_content?('Not eligible', count: 0)
+  end
+
+  test "eligible badge if recipient eligible" do
+    eligible_badge
+
+    3.times do |i|
+      @restrictions << create(:restriction)
+      @funding_stream = create(:funding_stream, :restrictions => @restrictions, :funders => [@funders[i]])
+      @eligibility = create(:eligibility, :recipient => @recipient, :restriction => @restrictions[i])
+    end
+
+    create_and_auth_user!(:organisation => @recipient)
+    visit '/funders'
+
+    assert page.has_content?('Eligible', count: 3)
+    assert page.has_content?('Not eligible', count: 0)
+  end
+
+  test "not eligible badge if recipient no eligible" do
+    eligible_badge
+
+    3.times do |i|
+      @restrictions << create(:restriction)
+      @funding_stream = create(:funding_stream, :restrictions => @restrictions, :funders => [@funders[i]])
+      @eligibility = create(:eligibility, :eligible => false, :recipient => @recipient, :restriction => @restrictions[i])
+    end
+
+    create_and_auth_user!(:organisation => @recipient)
+    visit '/funders'
+
+    assert page.has_content?('Eligible', count: 0)
+    assert page.has_content?('Not eligible', count: 3)
+  end
+
 end
