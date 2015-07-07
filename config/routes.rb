59171c20415df2136a2adf8572828f964e6b42ab
Suraj <contact@suninthesky.co.uk>
2015-07-07 17:05:25 +0100

    Add index path to profiles

--- config/routes.rb
+++ config/routes.rb
@@ -68,7 +68,7 @@ Rails.application.routes.draw do
       post :vote
       post :approach_funder
     end
-    resources :profiles, :only => [:new, :create, :edit, :update]
+    resources :profiles, :only => [:new, :create, :index, :edit, :update]
     resources :recipient_attribute, :as => :attribute, :only => [:new, :create, :edit, :update]
   end
 
