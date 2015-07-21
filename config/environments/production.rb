8ddb2f1ceef77480ad153ac762dffdc43ef4a7e4
Suraj <contact@suninthesky.co.uk>
2015-07-21 13:29:40 +0100

    Update mailer url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => "beehive-giving.herokuapp.com" }
-  config.action_mailer.asset_host = 'http://beehive-giving.herokuapp.com'
+  config.action_mailer.default_url_options = { :host => "beehivegiving.org" }
+  config.action_mailer.asset_host = 'http://www.beehivegiving.org'
 end
