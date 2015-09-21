f818e7bb7b8f1c773d9614e53cbde0e4a2bd6ab6
Suraj <contact@suninthesky.co.uk>
2015-09-21 14:05:47 +0100

    Need help snippet for profile multi step form

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Beehive
   class Application < Rails::Application
-    config.exceptions_app = self.routes
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -20,5 +19,11 @@ module Beehive
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.exceptions_app = self.routes
+
+    config.assets.precompile += %w(
+      modals.css
+    )
   end
 end
