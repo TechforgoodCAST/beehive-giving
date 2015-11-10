6edbc16a7378154d1027bd39a8d174965f7ba0aa
Suraj <contact@suninthesky.co.uk>
2015-11-10 20:56:41 +0000

    Update recommended funders design

--- app/helpers/recipients_helper.rb
+++ app/helpers/recipients_helper.rb
@@ -1,24 +1,14 @@
 module RecipientsHelper
 
+  # refactor?
   def dup_hash(ary)
     ary.inject(Hash.new(0)) { |h,e| h[e] += 1; h }.select {
       |k,v| v > 0 }.inject({}) { |r, e| r[e.first] = e.last; r }
   end
 
   def funder_card_cta_button_copy(recipient, funder)
-    classes = 'uk-width-1-1 uk-button uk-button-large uk-button-primary'
-
-    # if recipient.eligible?(funder)
-    #   content_tag(:a, link_to('Apply', '#', class: classes))
-    # elsif !recipient.eligible?(funder) && recipient.questions_remaining?(funder)
-    #   content_tag(:a, link_to('Check eligibility', recipient_eligibility_path(funder), class: classes))
-    # elsif !recipient.eligible?(funder) && recipient.eligibility_count(funder) > 0
-    #   content_tag(:a, link_to('Why ineligible?', '#', class: classes))
-    # else
-    #   content_tag(:a, link_to('Browse', recipient_comparison_path(funder), class: classes))
-    # end
-
     # Must run Recipient.joins(:users).find_each { |r| r.check_eligibilities } before
+    classes = 'uk-width-1-1 uk-button uk-button-primary uk-button-large'
     if recipient.load_recommendation(funder).eligibility
       if recipient.eligible?(funder)
         content_tag(:a, link_to('Apply', '#', class: classes))
