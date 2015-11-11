8c51ec5071dc02f154a5c3875106674baf4fc7c2
Suraj <contact@suninthesky.co.uk>
2015-11-11 12:02:36 +0000

    Recipients funders pages layout

--- app/helpers/recipients_helper.rb
+++ app/helpers/recipients_helper.rb
@@ -6,6 +6,19 @@ module RecipientsHelper
       |k,v| v > 0 }.inject({}) { |r, e| r[e.first] = e.last; r }
   end
 
+  def recipients_navbar_funders_active
+    current_page?(recommended_funders_path) ||
+    current_page?(eligible_funders_path) ||
+    current_page?(ineligible_funders_path) ||
+    current_page?(all_funders_path)
+  end
+
+  def recipients_navbar_my_nonprofit_active
+    current_page?(recipient_profiles_path(current_user.organisation)) ||
+    current_page?(edit_recipient_profile_path(current_user.organisation, current_user.organisation.profiles.first)) ||
+    current_page?(recipient_eligibilities_path(current_user.organisation))
+  end
+
   def funder_card_cta_button_copy(recipient, funder)
     # Must run Recipient.joins(:users).find_each { |r| r.check_eligibilities } before
     classes = 'uk-width-1-1 uk-button uk-button-primary uk-button-large'
@@ -18,6 +31,25 @@ module RecipientsHelper
     else
       content_tag(:a, link_to('Check eligibility', recipient_eligibility_path(funder), class: classes))
     end
+  end
+
+  def redacted_insights
+    [
+      'An epic insight of magnificence!',
+      'A golden nugget of knowledge.',
+      'That critical fact for decision making.',
+      'A morsel to muse over.',
+      'That timesaving tip.',
+      'A curious connection.',
+      'Awesome advice.'
+    ].sample
+  end
+
+  def redacted_actions
+    [
+      'More info',
+      'Show me more'
+    ].sample
   end
 
   def percentage(percent, total)
