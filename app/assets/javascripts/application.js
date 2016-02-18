1e85f0a1669376eb7a09b139aaee32319a19a164
Suraj <contact@suninthesky.co.uk>
2016-02-18 16:21:42 +0000

    Fix homepage floats

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,31 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.turbolinks
-//= require uikit
+
+//= require uikit/uikit
+//= require uikit/core/core
+//= require uikit/core/alert
+//= require uikit/core/button
+//= require uikit/core/cover
+//= require uikit/core/dropdown
+//= require uikit/core/grid
+//= require uikit/core/modal
+//= require uikit/core/nav
+//= require uikit/core/offcanvas
+//= require uikit/core/scrollspy
+//= require uikit/core/smooth-scroll
+//= require uikit/core/switcher
+//= require uikit/core/tab
+//= require uikit/core/toggle
+//= require uikit/core/touch
+//= require uikit/core/utility
+//= require uikit/components/form-password
+//= require uikit/components/form-select
+//= require uikit/components/grid
+//= require uikit/components/notify
+//= require uikit/components/sticky
+//= require uikit/components/tooltip
+
 //= require nprogress
 //= require nprogress-turbolinks
 //= require nprogress-ajax
