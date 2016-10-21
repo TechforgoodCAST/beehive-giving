a8bd7ef4f85414af58a9a1f16f66e1f3f261b707
Suraj <contact@suninthesky.co.uk>
2016-10-21 11:09:49 +0100

    Force SSL

--- config/initializers/staging_auth.rb
+++ config/initializers/staging_auth.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
     # Restrict access with HTTP Basic Auth for staging environments
     unless ENV['STAGING_AUTH'].blank?
+        config.force_ssl = false
+
         config.middleware.use '::Rack::Auth::Basic' do |username, password|
             ENV['STAGING_AUTH'].split(';').any? do |pair|
                 [username, password] == pair.split(':')
