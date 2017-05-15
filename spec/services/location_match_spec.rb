9bd5afa77771189d9d6d02728fa8cde3f3f399ef
Suraj Vadgama <contact@suninthesky.co.uk>
2017-05-15 13:12:28 +0100

    Update LocationMatch#initialize

--- spec/services/location_match_spec.rb
+++ spec/services/location_match_spec.rb
@@ -1,7 +1,35 @@
 require 'rails_helper'
 
-fdescribe LocationMatch do
-  it 'requires funds and proposal to initialize'
+describe LocationMatch do
+  context 'init' do
+    before(:each) do
+      @app.seed_test_db.setup_funds.create_recipient.create_registered_proposal
+      @funds = Fund.active.all
+      @proposal = Proposal.last
+    end
+
+    it 'requires funds and proposal to initialize' do
+      expect { LocationMatch.new }.to raise_error(ArgumentError)
+      expect { LocationMatch.new(@funds) }.to raise_error(ArgumentError)
+      expect { LocationMatch.new(@funds, @proposal) }.not_to raise_error
+    end
+
+    it 'invalid funds collection raises errror' do
+      expect { LocationMatch.new({}, @proposal) }
+        .to raise_error('Invalid Fund::ActiveRecord_Relation')
+    end
+
+    it 'empty funds collection raises errror' do
+      Fund.destroy_all
+      expect { LocationMatch.new(@funds, @proposal) }
+        .to raise_error('No funds supplied')
+    end
+
+    it 'invalid proposal object raises error' do
+      expect { LocationMatch.new(@funds, {}) }
+        .to raise_error('Invalid Proposal object')
+    end
+  end
 
   context 'fund seeking work in other countries' do
     it 'fund ineligible for proposal does not match countries'
