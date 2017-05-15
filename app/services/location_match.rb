c666ce78d865196401df719205e977be98305a5b
Suraj Vadgama <contact@suninthesky.co.uk>
2017-05-15 13:12:28 +0100

    Update LocationMatch#initialize

--- app/services/location_match.rb
+++ app/services/location_match.rb
@@ -2,5 +2,15 @@ class LocationMatch
   def initialize(funds, proposal)
     @funds = funds
     @proposal = proposal
+    validate_arguments
   end
+
+  private
+
+    def validate_arguments
+      raise 'Invalid Fund::ActiveRecord_Relation' unless
+        @funds.instance_of? Fund::ActiveRecord_Relation
+      raise 'No funds supplied' if @funds.empty?
+      raise 'Invalid Proposal object' unless @proposal.instance_of? Proposal
+    end
 end
