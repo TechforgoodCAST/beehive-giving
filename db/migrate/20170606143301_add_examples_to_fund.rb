33e4e2a9ab1aa78dd559c590f7e4d4872203fe40
David Kane <d.r.kane@gmail.com>
2017-06-06 16:07:44 +0100

    Change migration to add default of []

--- db/migrate/20170606143301_add_examples_to_fund.rb
+++ db/migrate/20170606143301_add_examples_to_fund.rb
@@ -1,5 +1,5 @@
 class AddExamplesToFund < ActiveRecord::Migration[5.0]
   def change
-    add_column :funds, :grant_examples, :jsonb
+    add_column :funds, :grant_examples, :jsonb, null: false, default: []
   end
 end
