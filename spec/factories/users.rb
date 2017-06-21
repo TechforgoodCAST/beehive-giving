dd76576ce5095e4ed705eb51d05584328e402a06
David Kane <d.r.kane@gmail.com>
2017-06-21 16:57:21 +0100

    Turn user org_type to int

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
     first_name 'John'
     last_name 'Doe'
-    org_type '0'
+    org_type 0
     sequence(:email) { |n| "email#{n}@organisation.org" }
     password 'password123'
     role 'User'
