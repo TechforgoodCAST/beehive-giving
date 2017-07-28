374fcf87d8ee4b6e218c7ca967d231df8e8a279b
David Kane <d.r.kane@gmail.com>
2017-07-28 14:15:00 +0100

    Changes to markdown - allow raw html, add tables and footnotes

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,8 +2,9 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def markdown(str)
-    options = { filter_html: true, hard_wrap: true,
+    options = { hard_wrap: true,
                 space_after_headers: true, fenced_code_blocks: true,
+                tables: true, footnotes: true, 
                 link_attributes: { target: '_blank' } }
 
     extensions = { autolink: true, disable_indented_code_blocks: true }
