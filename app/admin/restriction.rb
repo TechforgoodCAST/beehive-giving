4cc6814a970bc44b90dcb96734b3fe8a77835d4a
David Kane <d.r.kane@gmail.com>
2017-07-28 18:22:44 +0100

    Changes to admin interface

--- app/admin/restriction.rb
+++ app/admin/restriction.rb
@@ -1,10 +1,12 @@
 ActiveAdmin.register Restriction do
-  permit_params :restriction, :details, :invert
+  permit_params :restriction, :details, :invert, :category
 
   form do |f|
     f.inputs do
       f.input :details, label: 'Are you seeking funding for ___ ?'
       f.input :invert
+      f.input :category, collection: %w(Proposal Organisation),
+                         input_html: { class: 'chosen-select' }
     end
     f.actions
   end
@@ -12,6 +14,7 @@ ActiveAdmin.register Restriction do
   index do
     column :details
     column :invert
+    column :category
     actions
   end
 end
