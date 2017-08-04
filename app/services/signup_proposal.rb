959faa457e16e2f0259ff282735f2e6d3f7da69f
Suraj Vadgama <contact@suninthesky.co.uk>
2017-08-04 07:53:54 +0100

    Update #transfer_data!

--- app/services/signup_proposal.rb
+++ app/services/signup_proposal.rb
@@ -1,4 +1,31 @@
 class SignupProposal
+  MAPPING = {
+    'The general public' => 'Public and societal benefit',
+    'Affected or involved with crime' => 'Crime and justice',
+    'With family/relationship challenges' => 'Family and relationships',
+    'With disabilities' => 'Disability',
+    'With specific religious/spiritual beliefs' => 'People with a specific religious/spiritual beliefs',
+    'Affected by disasters' => 'Disaster preparedness and relief',
+    'In education' => 'Education and training',
+    'Who are unemployed' => 'Employment',
+    'From a specific ethnic background' => 'People from a specific ethnic background',
+    'With water/sanitation access challenges' => 'Water, sanitation and public infrastructure',
+    'With food access challenges' => 'Food and agriculture',
+    'With housing/shelter challenges' => 'Housing and shelter',
+    'Animals and wildlife' => 'Animals and wildlife',
+    'Buildings and places' => 'Building and places',
+    'With mental diseases or disorders' => 'Mental wellbeing, diseases and disorders',
+    'With a specific sexual orientation' => 'People with a specific gender or sexual identity',
+    'Climate and the environment' => 'Climate and the environment',
+    'With physical diseases or disorders' => 'Physical wellbeing, diseases and disorders',
+    'Organisations' => 'Organisational development',
+    'Facing income poverty' => 'Poverty',
+    'Who are refugees and asylum seekers' => 'Migration, refugees and asylum seekers',
+    'Involved with the armed or rescue services' => 'Armed and rescue services',
+    'In, leaving, or providing care' => 'People in, leaving or providing care',
+    'At risk of sexual exploitation, trafficking, forced labour, or servitude' => 'Human rights and exploitation'
+  }.freeze
+
   def initialize(organisation)
     @organisation = organisation
     @proposal = @organisation.proposals.new(
@@ -26,7 +53,6 @@ class SignupProposal
 
     def transfer_data!(profile) # TODO: refactor
       set_boolean(profile, 'People', :affect_people)
-      set_boolean(profile, 'Other', :affect_other)
 
       if @proposal.affect_people?
         @proposal.gender = profile.gender
@@ -42,22 +68,20 @@ class SignupProposal
         end
       end
 
-      %w(
-        beneficiaries_other_required
-        beneficiaries_other
-        implementations_other_required
-        implementations_other
-      ).each do |field|
+      %w[implementations_other_required implementations_other].each do |field|
         @proposal[field] = profile[field]
       end
-      @proposal.affect_other = true if @proposal.beneficiaries_other_required?
 
       @proposal.assign_attributes(
-        beneficiary_ids: profile.beneficiary_ids,
         implementation_ids: profile.implementation_ids,
         country_ids: profile.country_ids,
-        district_ids: profile.district_ids
+        district_ids: profile.district_ids,
+        themes: get_themes(profile.beneficiaries)
       )
+    end
+
+    def get_themes(beneficiaries)
+      Theme.where name: beneficiaries.map { |b| MAPPING[b.label] }
     end
 
     def set_boolean(profile, category, field)
--- lib/tasks/proposals.rake
+++ lib/tasks/proposals.rake
@@ -1,15 +1,12 @@
 namespace :proposals do
-  # usage: rake proposals:migrate_themes! MAPPING=/path/to/file.json SAVE=true
-  # MAPPING JSON format: { <Beneficiary.name>: <Theme.name>, ... }
+  # usage: rake proposals:migrate_themes! SAVE=true
   desc 'Update specified funds'
   task migrate_themes!: :environment do
-    mapping = open(ENV['MAPPING']) { |file| JSON.parse(file.read) }
-
     Proposal.skip_callback(:save, :after, :initial_recommendation)
 
     Proposal.joins(:beneficiaries).distinct.find_each do |proposal|
       proposal.themes = Theme.where(
-        name: proposal.beneficiaries.map { |b| mapping[b.label] }
+        name: proposal.beneficiaries.map { |b| SignupProposal::MAPPING[b.label] }
       )
       proposal.save!(validate: false) if ENV['SAVE']
       print '.'
