88177f7ecfeb40b9b53d06080e0f72f5872171ec
David Kane <d.r.kane@gmail.com>
2017-08-07 09:25:29 +0100

    Don't focus tests

--- spec/services/check_suitability/check_theme_spec.rb
+++ spec/services/check_suitability/check_theme_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-fdescribe CheckSuitability::CheckTheme do
+describe CheckSuitability::CheckTheme do
 
   before(:each) do
     @app.seed_test_db.setup_funds.create_recipient.create_registered_proposal
