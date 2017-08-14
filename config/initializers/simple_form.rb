3872ffb79ec6753b4eb94b6ab5f0d6a5f1c4a316
Suraj Vadgama <contact@suninthesky.co.uk>
2017-08-14 15:56:50 +0100

    Simple styling user sign up form

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -5,7 +5,7 @@ SimpleForm.setup do |config|
   # wrapper, change the order or even add your own to the
   # stack. The options given below are used to wrap the
   # whole input.
-  config.wrappers :default, class: :input,
+  config.wrappers :default, class: :input_wrapper,
                             hint_class: :field_with_hint, error_class: :field_with_errors do |b|
     ## Extensions enabled by default
     # Any of these extensions can be disabled for a
@@ -42,7 +42,7 @@ SimpleForm.setup do |config|
     ## Inputs
     b.use :input
     # b.use :hint,  wrap_with: { tag: :span, class: :hint }
-    # b.use :error, wrap_with: { tag: :span, class: :error }
+    b.use :error, wrap_with: { tag: :span, class: :error }
 
     ## full_messages_for
     # If you want to display the full error message for the attribute, you can
