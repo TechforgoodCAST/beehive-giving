63a4137787e571da0e09424cfc0411bed57b8e52
David Kane <d.r.kane@gmail.com>
2017-08-18 18:34:53 +0100

    Add descriptions for each ineligible reason

--- app/cells/eligibility_banner_cell.rb
+++ app/cells/eligibility_banner_cell.rb
@@ -4,6 +4,22 @@ class EligibilityBannerCell < Cell::ViewModel
     render locals: { fund: options[:fund] }
   end
 
+  def location
+    render locals: {}
+  end
+
+  def amount
+    render locals: {}
+  end
+
+  def org_type
+    render locals: { org_type: ORG_TYPES[model.recipient.org_type + 1][2] }
+  end
+
+  def quiz
+    render locals: { count_failing: model.eligibility[options[:fund]&.slug]['quiz']['count_failing'] }
+  end
+
   private
 
     def ineligible_because(key)
