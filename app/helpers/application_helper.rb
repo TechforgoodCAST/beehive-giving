c5ef6a04bcefab538299c8eb69292633b8f85fcc
Suraj Vadgama <contact@suninthesky.co.uk>
2017-08-22 15:44:50 +0100

    New style pages#forfunders

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
   def v2_layout? # TODO: remove @ v2
     permitted = {
       articles: %i[index show],
-      pages: %i[about faq privacy terms],
+      pages: %i[about faq forfunders privacy terms],
       signup: %i[user create_user]
     }
     controller = params[:controller].to_sym
