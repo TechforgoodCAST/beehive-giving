d276349e53fc6c712c749a647c6a101a2401eee5
David Kane <d.r.kane@gmail.com>
2017-10-11 22:37:28 +0100

    Update suitability cell

--- app/cells/suitability_cell.rb
+++ app/cells/suitability_cell.rb
@@ -2,8 +2,19 @@ class SuitabilityCell < Cell::ViewModel
   include ActionView::Helpers::FormHelper
   include ActionView::Helpers::FormOptionsHelper
   include SimpleForm::ActionViewExtensions::FormHelper
+  include ActionView::Helpers::NumberHelper
+
+  LOCATION_MESSAGES = {
+    'anywhere'  => 'Accepts proposals from <strong>anywhere</strong> in the country.',
+    'exact'     => 'Supports <strong>all</strong> of the areas in your proposal.',
+    'intersect' => 'Supports <strong>some</strong> of the areas in your proposal.',
+    'partial'   => 'Supports <strong>all</strong> of the areas in your proposal.',
+    'overlap'   => 'Supports projects in a <strong>smaller</strong> area than you are seeking.',
+    'national'  => 'Supports <strong>national</strong> proposals.'
+  }.freeze
 
   def page
+    return unless options[:fund].priorities.present?
     render locals: { fund: options[:fund], status: status, criteria: criteria }
   end
 
@@ -12,6 +23,10 @@ class SuitabilityCell < Cell::ViewModel
   end
 
   def index
+    render locals: { fund: options[:fund], status: status, criteria: criteria }
+  end
+
+  def analysis
     render locals: { fund: options[:fund], status: status, criteria: criteria }
   end
 
@@ -38,38 +53,62 @@ class SuitabilityCell < Cell::ViewModel
 
     def criteria_status(criteria)
         criteria.each do |k, v|
-            score = model.suitability[options[:fund].slug]&.dig(k.to_s, "score") || 0
+            score = model.suitability[options[:fund].slug]&.dig(k.to_s, "score")
+            reason = model.suitability[options[:fund].slug]&.dig(k.to_s, "reason")
             status = score_to_status(score)
             v[:colour] = status[:colour]
             v[:symbol] = status[:symbol]
             v[:status] = status[:title]
-            v[:message] = status[:title]
+            v[:message] = message(k, score, reason)
         end
     end
 
     def score_to_status(score, scale = 1)
+        return {
+            score: nil, 
+            title: 'Unavailable', 
+            colour: 'blue', 
+            symbol: "<span class=\"white dot dot-14 bg-blue mr3\"></span>".html_safe
+        } if score == nil
         scale = score > 1 ? score.ceil : 1
         [
-            {score: 0, title: 'Unavailable', colour: 'blue', symbol: "<span class=\"white dot dot-14 bg-blue mr3\"></span>".html_safe},
-            {score: 0.2, title: 'Very poor', colour: 'red', symbol: "\u2718".html_safe},
-            {score: 0.4, title: 'Poor', colour: 'red', symbol: "\u2718".html_safe},
-            {score: 0.6, title: 'Fair', colour: 'blue', symbol: "~"},
-            {score: 0.8, title: 'Good', colour: 'green', symbol: "\u2714".html_safe},
-            {score: 1.0, title: 'Excellent', colour: 'green', symbol: "\u2714".html_safe},
+            {score: 0.2, title: 'Unsuitable', colour: 'red', symbol: "\u2718".html_safe},
+            {score: 0.5, title: 'Suitability', colour: 'yellow', symbol: "~"},
+            {score: 1.0, title: 'Suitable', colour: 'green', symbol: "\u2714".html_safe},
         ].each do |v|
             return v if score <= (v[:score] * scale)
         end
     end
 
     def status
-        score = model.suitability[options[:fund].slug]&.dig("total") || 0
+        score = model.suitability[options[:fund].slug]&.dig("total")
         status = score_to_status(score)
-        {
-            status: (score < 0.5 ? "Unsuitable" : "Suitable"),
-            colour: status[:colour],
-            symbol: status[:symbol],
-            link_text: "Find out more" 
-        }
+        status[:link_text] = "Find out more"
+        status[:status] = status[:title]
+        status
+    end
+
+    def message(criteria, score, reason)
+        case criteria
+        when :amount
+            return "No grants for a similar amount." if score == 0
+            "#{number_to_percentage(score * 100, precision: 0)} of grants were for a similar amount."
+        when :location
+            LOCATION_MESSAGES[reason]
+        when :org_type
+            org_type = ORG_TYPES[model.recipient.org_type + 1][2]
+            return "No grants to #{org_type}." if score == 0
+            "#{number_to_percentage(score * 100, precision: 0)} of grants were to #{org_type}."
+        when :duration
+            return "No grants for a similar length." if score == 0
+            "#{number_to_percentage(score * 100, precision: 0)} of grants were for a similar length."
+        when :theme
+            themes = model.themes.map{ |t| t.name } & options[:fund].themes.map{ |t| t.name }
+            return "No themes in common with your proposal" if themes.size == 0
+            "This fund works in #{themes.take(3).to_sentence}."
+        when :quiz
+            "#{number_to_percentage(score * 100, precision: 0)} of grants were for a similar amount."
+        end
     end
     
     def protect_against_forgery?
