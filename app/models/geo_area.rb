cf89923725849f09d5a1bf50156e9f40dd329f10
David Kane <d.r.kane@gmail.com>
2017-10-24 16:41:42 +0100

    Add districts check to geo_area

--- app/models/geo_area.rb
+++ app/models/geo_area.rb
@@ -7,7 +7,19 @@ class GeoArea < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :countries, presence: true
 
+  validate :validate_districts
+
   def short_name
     self[:short_name].present? ? self[:short_name] : self[:name]
   end
+
+  private
+
+    def validate_districts
+      return if districts.empty?
+      districts.each do |district|
+        errors.add(:districts, "Country '#{district.country.name}' for #{district.name} not selected") if
+          country_ids.exclude?(district.country_id)
+      end
+    end
 end
