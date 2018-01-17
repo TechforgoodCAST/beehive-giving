b18d33fd68da5d5c2af7f93abed43c6aa5a610a0
Suraj Vadgama <contact@suninthesky.co.uk>
2018-01-17 16:43:43 +0000

    v1.7.4

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
-    scope :grouped, ->(type, group) { where(criterion_type: type, group: group) }
-    belongs_to :criterion, polymorphic: true, dependent: :destroy
-    belongs_to :fund
-    has_many :answers, through: :criterion
+  scope :grouped, ->(type, group) { where(criterion_type: type, group: group) }
+  belongs_to :criterion, polymorphic: true
+  belongs_to :fund
+  has_many :answers, through: :criterion
 end
