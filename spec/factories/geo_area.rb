67aec34bc8e283b96a31781c0c99c77f07308efe
Suraj Vadgama <contact@suninthesky.co.uk>
2018-09-24 17:15:31 +0100

    GeoArea specs passing

--- spec/factories/geo_area.rb
+++ spec/factories/geo_area.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :geo_area do
     sequence(:name) { |n| "Area #{n}" }
+
+    after(:build) do |area, _evaluator|
+      country = build(:country)
+      area.countries = [country]
+      area.districts = build_list(:district, 1, country: country)
+    end
   end
 end
