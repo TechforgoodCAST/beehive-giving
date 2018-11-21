1aaf60e5542e3c299e39f45544ca0a4183e9b9ef
Suraj Vadgama <contact@suninthesky.co.uk>
2018-11-21 14:22:17 +0000

    Article does not titleize title

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,15 +2,11 @@ class Article < ApplicationRecord
   validates :title, :slug, :body, presence: true
   validates :slug, uniqueness: true
 
-  def to_param
-    slug
-  end
-
-  def title=(str)
-    self[:title] = str&.titleize
-  end
-
   def body_html
     markdown(body)
+  end
+
+  def to_param
+    slug
   end
 end
